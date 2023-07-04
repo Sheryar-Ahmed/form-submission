7b488425d0d3c562853da601497f97bb891d4fd4
Royal <98022295+Sheryar-Ahmed@users.noreply.github.com>
2023-07-04 17:43:39 +0500

    Update index.js

--- index.js
+++ index.js
@@ -15,30 +15,3 @@ app.use(errorHanlder);
 app.listen(3000, () => {
     console.log('Server listening on port 3000');
 });
-
-
-
-
-
-
-
-
-
-
-// app.use((req, res, next)=>{
-//     const err = new Error("Not found");
-//     err.status = 404;
-//     next(err);
-// });
-
-// app.use((err, req, res, next)=>{
-//     if(err.isJoi) err.status = 422;
-//     return res
-//     .status(err.status || 500)
-//     .send({
-//         error: {
-//             status : err.status || 500,
-//             message : err.message
-//         }
-//     });
-// })
