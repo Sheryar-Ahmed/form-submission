1f25c09a02fb88fdef62bff8fdb5bf9c974e17c0
Sheryar Ahmed <136733943+sheryarahmedse@users.noreply.github.com>
2023-07-06 23:11:09 +0500

    added cors

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 const express = require('express');
 const formRoute = require('./routes/formRoute');
 const { errorHanlder } = require('./middleware/errorMiddleware');
+const cors = require('cors');
 require('dotenv').config();
+
 const PORT = 5001;
 const app = express();
+
 app.use(express.json());
+
+// Enable CORS for all requests and methods
+app.use(cors({
+  origin: '*',
+  methods: '*'
+}));
+
 // home url check
 app.get('/', (req, res) => {
-    res.send('Lead oqvest api working fine...');
+  res.send('Lead oqvest api working fine...');
 });
+
 app.use('/api/v1', formRoute);
 app.use(errorHanlder);
 
 app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
+  console.log(`Server listening on port ${PORT}`);
 });
